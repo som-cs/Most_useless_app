a2ea8deb0502169d710999a7f5f5fcb383fac7ea
som <sudhanshu253@gmail.com>
2022-02-14 15:40:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mostuselessapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mostuselessapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
